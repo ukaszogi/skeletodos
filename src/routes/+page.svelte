644dece3739d62c4e642de255def9b18697c3480
ukaszogi <ukaszogi@gmail.com>
2023-11-20 23:47:29 +0100

    Miserably little done today

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,59 @@
 <script lang="ts">
 	let todo_task = '';
-	let todos = {
-		
-	}
+	let todos = [
+    {
+      id: 0,
+      todo_task: "Helena",
+      todo_done: false,
+      created_date: new Date()
+    },
+    {
+      id: 1,
+      todo_task: "Obsraj spodnie",
+      todo_done: true,
+      created_date: new Date()
+    }
+	]
 
-	function handleKeydown(e: KeyboardEvent) {
+	function add_todo(e: KeyboardEvent) {
 		// console.log(e, todo_task);
-		let element: HTMLInputElement = e.target; //śmieszny error, nic nie robi, wymieniłem dwa errory na jeden
+		const element: HTMLInputElement = e.target; //śmieszny error, nic nie robi, wymieniłem dwa errory na jeden
 		if (e.key == 'Enter') {
-			console.log('No tu będzie dodawany todo z', todo_task); //już wiem jak naprawić, ale nadal nie wiem czemu
+			// console.log('No tu będzie dodawany todo z', todo_task);
+      todos = [{
+        id: window.crypto.randomUUID(),
+        todo_task: todo_task,
+        todo_done: false,
+        created_date: new Date()
+      }, ...todos]
 			element.placeholder = 'Może więcej dozrobieniusów, co?';
-			todo_task = ''; //trzeba było nie używać skróconej formy do input:text jako {value}
+			todo_task = '';
 		}
 	}
 
-	// $: console.log(todo_task);
+
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h2 class="h2">Czuj się wolny by dodać dozrobienia!</h2>
 		<label class="label">
-			Treść Twojej supermisji
+			Treść Twojej supermisji (supremacji Twojego chłopaka)
 			<input
 				bind:value={todo_task}
-				on:keydown={handleKeydown}
-				class="input"
+				on:keydown={add_todo}
+				class="input w-50"
 				type="text"
 				name="content"
 				id="content-input"
 				placeholder="Dodaj dozrobienia bracie"
 			/>
 		</label>
+    {#each todos as todo}
+      <input 
+        class="input w-50 {todo.todo_done ? 'variant-soft-success' : 'variant-soft-error'}"
+        value={todo.todo_task} 
+      />
+    {/each}
 	</div>
 </div>

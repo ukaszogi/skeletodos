5f4ab9f1021c92b5cd7acb5005855fe20eb4fb69
ukaszogi <ukaszogi@gmail.com>
2023-11-20 07:49:56 +0100

    Added AppBär und AppSchell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,28 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	// Components
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">📅</svelte:fragment>
+			<h1 class="h1">No nie, tylko nie kolejna aplikacja z tudusami</h1>
+			<svelte:fragment slot="trail" >
+				<span class="text-3xl">😭😭😭</span>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
